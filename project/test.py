51e77bd361ca0eae1e4cfc62486cffbade24e62b
archon1999 <nazarbek.baltabaev@gmail.com>
2023-05-02 13:59:40 +0500

    changes

--- project/test.py
+++ project/test.py
@@ -4,12 +4,6 @@ import django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
 django.setup()
 
-from backend.models import Problem, Attempt, User, Tag
+from backend.models import Problem
 
-import random
 print(Problem.objects.first())
-
-tags = list(Tag.objects.all().values_list('id', flat=True))
-for problem in Problem.objects.all():
-    for _ in range(random.randint(0, 4)):
-        problem.tags.create(tag_id=random.choice(tags))
